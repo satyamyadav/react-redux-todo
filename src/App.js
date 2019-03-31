312b3f4dd506a1b8d268372f119c245c71f26ada
Satyam Yadav <satyamyadav3@gmail.com>
2019-03-31 21:35:56 +0530

    feat: adds container component visible todolist

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import './components/TodoList';
-import TodoList from './components/TodoList';
+import VisibleTodoList from './containers/VisibleTodoList';
 import Footer from './components/Footer';
 
-// static todo list
-const staticTodos = [
-  { 
-    id: 0,
-    "text": "this is first task added",
-    "completed": true
-  },
-  { 
-    id: 1,
-    "text": "second one",
-    "completed": false
-  },
-  {
-    id: 2,
-    "text": "third added",
-    "completed": false
-  }
-]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList todos={staticTodos} onTodoClick={() => { console.log('todo clicked') }} />
+        <VisibleTodoList />
         <Footer />
       </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -20,7 +20,7 @@ const TodoList = ({ todos, onTodoClick }) => (
 TodoList.propTypes = {
   onTodoClick: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    // id: PropTypes.number.isRequired,
     completed: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired,
   })).isRequired,
