b8c7e5c8e28253b9a0892b78663d0826ec2ccd10
Satyam Yadav <satyamyadav3@gmail.com>
2019-04-01 15:58:11 +0530

    build: build

---
+++ build/precache-manifest.2ba4610654c7b14c42fbe6130419ebb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "c888a911d9f672729992",
+    "url": "/build/static/js/main.c2476392.chunk.js"
+  },
+  {
+    "revision": "7d2c3bf6ecf216238d69",
+    "url": "/build/static/js/2.b954ef16.chunk.js"
+  },
+  {
+    "revision": "c888a911d9f672729992",
+    "url": "/build/static/css/main.d679b939.chunk.css"
+  },
+  {
+    "revision": "99f8d09b9f7f6b9cb88775daa254d982",
+    "url": "/build/index.html"
+  }
+];
